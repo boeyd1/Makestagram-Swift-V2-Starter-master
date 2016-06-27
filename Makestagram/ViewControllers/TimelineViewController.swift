2aa4150d8013fa2dfd1bd2a41bbd8788875d8b67
boeyd1 <dominic.boey@gmail.com>
2016-06-27 17:56:37 +0800

    completed part 8
    adding functionalities for photoviewcontroller mainly

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -9,27 +9,38 @@
 import UIKit
 
 class TimelineViewController: UIViewController {
-
+    
+    var photoTakingHelper: PhotoTakingHelper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.tabBarController?.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        //without a trailing closure, it will look like this ------>
+        //PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
+        
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            // don't do anything, yet...
+            print("received a callback")
+        }
     }
-    */
-
 }
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
